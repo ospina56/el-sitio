1f96dcf20120527d7c7bd8fb2e0ddaa3c3668134
ospina56 <128724094+ospina56@users.noreply.github.com>
2024-07-18 12:48:21 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>FASHION WORLD</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
+    <link rel="stylesheet" href="libs/fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="libs/css/bootstrap.min.css">
+    <link rel="stylesheet" href="libs/css/magnific-popup.css">
+    <link rel="stylesheet" href="libs/css/jquery-ui.css">
+    <link rel="stylesheet" href="libs/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="libs/css/owl.theme.default.min.css">
+
+
+    <link rel="stylesheet" href="libs/css/aos.css">
+
+    <link rel="stylesheet" href="libs/css/style.css">
+    
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+    <header class="site-navbar" role="banner">
+      
+      <nav class="site-navigation text-right text-md-center" role="navigation">
+        
+          <div class="row">
+        <div class="col-3">
+          <img src="imagenes\logo.png" alt="" width="80px" height="80px">
+        </div>
+        <div class="col-9">
+          <ul class="site-menu js-clone-nav d-none d-md-block">
+            
+            <li >
+              <a href="index.html">Home</a>
+             
+            </li>
+            
+            <li><a href="Categorias.php">Categorias</a></li>
+            <li><a href="#">Fechas Especiales</a></li>
+            <li><a href="#">Lista de deseos</a></li>
+            <li><a href="#">Perfil</a></li>
+            <li><a href="contact.html">Contacto</a></li>
+            <li>
+              <div class="site-top-icons">
+                <ul>
+                  <li><a href="seccion1.php"><span class="icon icon-person"></span></a></li>
+                  <li><a href="seccion2.php"><span class="icon"><img src="imagenes\agregar-usuario.png"></a></li>
+                  <li><a href="seccion8.php"><span class="icon icon-heart"></span></a></li>
+                  <li>
+                    <a href="view/catalogo.php" class="site-cart">
+                      <span class="icon icon-shopping_cart"></span>
+                      <span class="count">0</span>
+                    </a>
+                  </li> 
+                  <li class="d-inline-block d-md-none ml-md-0"><a href="#" class="site-menu-toggle js-menu-toggle"><span class="icon-menu"></span></a></li>
+                </ul>
+              </div> 
+              </li>
+          </ul>
+          </div>
+          </div>
+        
+      </nav>
+    </header>
+
+    <div class="site-blocks-cover" style="background-image: url(imagenes/fondo.jpg);" data-aos="fade">
+      <div class="container">
+        <div class="row align-items-start align-items-md-center justify-content-end">
+          <div class="col-md-5 text-center text-md-left pt-5 pt-md-0">
+            <h1 class="mb-2"><i>FASHION WORLD</i></h1>
+            <div class="intro-text text-center text-md-left">
+              <p>
+                <a href="view/catalogo.php" class="btn btn-sm btn-warning">Comprar</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section block-8">
+      <div class="container">
+        <div class="row justify-content-center  mb-5">
+          <div class="col-md-7 site-section text-center pt-4">
+            <h2>Bienvenidos</h2>
+          </div>
+        </div>
+        <div class="row align-items-center">
+          <div class="col-md-12 col-lg-7 mb-5">
+            <a href="#"><img src="imagenes\ropa-todos.jpg" alt="Image placeholder" class="img-fluid rounded"></a>
+          </div>
+          <div class="col-md-12 col-lg-5 text-center pl-md-5">
+            <h2>VENDEMOS</h2>
+            <p>ROPA, ACESORIOS, ZAPATOS Y MÁS</p>
+            <p><a href="view/catalogo.php" class="btn btn-dark btn-sm">Ver más</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section site-blocks-2">
+      <div class="container"><center><h1>Categorias</center></h1><br>
+        <div class="row">
+          <div class="col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0" data-aos="fade" data-aos-delay="">
+            <a class="block-2-item" href="Categorias-mujer.php">
+              <figure class="image">
+                <img src="imagenes\ropa-mujer.png" alt="" class="img-fluid">
+              </figure>
+              <div class="text">
+                <span class="text-uppercase">Categorias</span>
+                <h3>Mujeres</h3>
+              </div>
+            </a>
+          </div>
+          <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+            <a class="block-2-item" href="Categorias-niños.php">
+              <figure class="image">
+                <img src="imagenes\ropa-niños.jpg" alt="" class="img-fluid">
+              </figure>
+              <div class="text">
+                <span class="text-uppercase">categorias</span>
+                <h3>Niños</h3>
+              </div>
+            </a>
+          </div>
+          <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="200">
+            <a class="block-2-item" href="Categorias-hombre.php">
+              <figure class="image">
+                <img src="imagenes\ropa-hombres.jpg" alt="" class="img-fluid">
+              </figure>
+              <div class="text">
+                <span class="text-uppercase">Categorias</span>
+                <h3>Hombres</h3>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <footer class="site-footer border-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 mb-5 mb-lg-0">
+          <div class="block-5 mb-5">
+              <h3 class="footer-heading mb-4">Información de Contacto</h3>
+              <ul class="list-unstyled">
+                <li class="address">San José del Guaviare, Guaviare</li>
+                <li class="phone"><a href="tel://57 3152802997">+57 3152802997</a></li>
+                <li class="email">fashionworld@gmail.com</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
+            <h3 class="footer-heading mb-4">Mas Productos</h3>
+            <a href="view/Categorias.php" class="block-6">
+              <img src="imagenes\logo.png" alt="Image placeholder" class="img-fluid rounded mb-4" height="20px">
+              
+              
+            </a>
+          </div>
+          <div class="col-md-6 col-lg-3">
+        
+          </div>
+        </div>
+        
+      </div>
+    </footer>
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
